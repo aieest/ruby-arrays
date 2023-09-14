d7014e0783b1f1f440ecf2aa3cb61f50d7e796f3
August Estidola <aie.estidola@gmail.com>
2023-09-14 13:42:13 +0800

    Fixed the grammar and format

--- array_1.rb
+++ array_1.rb
@@ -1,12 +1,12 @@
-#Let's keep the first challenge simple
-#Your is to print the number divisible by either 3, 5, or 7
+# Let's keep the first challenge simple
+# Your task is to print the number divisible by either 3, 5, or 7
 
 numbers = Array[34, 24, 48, 19, 23, 81]
 
-#type your code below your line
+# type your code below your line
 
 numbers.each do |num|
-    if num % 3 == 0 || num % 5 == 0 || num % 7 == 0
-      puts num
-    end
+  if num % 3 == 0 || num % 5 == 0 || num % 7 == 0
+    puts num
   end
+end
